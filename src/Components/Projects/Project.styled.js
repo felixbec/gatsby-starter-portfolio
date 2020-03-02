7e604c28f14100c3b0922e9439bf5af956960608
Carlos <cfelixbeltran@dynamicagency.com>
2020-03-01 18:26:27 -0600

    FIXED alternating project objects styles

--- src/Components/Projects/Project.styled.js
+++ src/Components/Projects/Project.styled.js
@@ -4,37 +4,96 @@ const StyledContainer = styled.div`
     margin: 5rem 0;
     h1{color: ${({ theme }) => theme.textActive};}
     p{color: ${({ theme }) => theme.accentText}; font-size: 1.5rem;}
+
+    /*@media (min-width: 768px) {
+        .project-holder:nth-child(2n){
+            .row{
+                .col-md-8{
+                    order: 2;
+                    background: ${({ theme }) => theme.bgGradientRight}, url(${props => props.background});
+                }
+                .col-md-4{
+                    .information{
+                        margin-left: 0;
+                        margin-right: -50%;
+                    }
+                    div{
+                        align-items: flex-end;
+                    }
+                }
+            }
+        }
+    }*/
 `
 
 const StyledProject = styled.div`
     width: 100%;
-    margin: 5rem 0;
-    
+    margin: 5rem 0!important;
 `
 
 const StyledImage = styled.div`
     background: ${({ theme }) => theme.bgGradient}, url(${props => props.background});
     height: 400px;
     background-size: contain;
+    @media (max-width: 768px) {
+        height: 250px;
+    }
+    @media (max-width: 992px) and (min-width: 768px) {
+        height: 350px;
+    }
+`
+const StyledImageAlt = styled(StyledImage)`
+    order:2;
+    background: ${({ theme }) => theme.bgGradientRight}, url(${props => props.background});
+    background-size: contain;
+    @media (max-width: 768px) {
+            order: 0;
+        }
 `
 
 const StyledContent = styled.div`
     display: flex;
     flex-direction: column;
 
+    @media (max-width: 768px) {
+        text-align: right;
+        padding: 10px;
+    }
+
     div{
         height: 50%;
         display: flex;
         flex-direction: column;
         justify-content: center;
+
+        @media (max-width: 768px) {
+                height: auto;
+            }
     }
     .information{
         margin-left:-50%;
+        @media (max-width: 768px) {
+                margin-left: 0;
+            }
         h3{color: ${({ theme }) => theme.textActive};}
         p{color: ${({ theme }) => theme.accentText};}
-
     }
     
+`
+
+const StyledContentAlt = styled(StyledContent)`
+    .information{
+        margin-left: 0;
+        margin-right: -50%;
+        z-index:1;
+        @media (max-width: 768px) {
+            margin: 0;
+        }
+    }
+    div{
+        align-items: flex-end;
+    }
+
 `
 
 const StyledLinkable = styled.a`
@@ -55,4 +114,4 @@ const StyledLinkable = styled.a`
     }
 `
 
-export {StyledContainer, StyledProject, StyledImage, StyledContent, StyledLinkable};
+export {StyledContainer, StyledProject, StyledImage, StyledImageAlt, StyledContent, StyledContentAlt, StyledLinkable};
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { graphql, useStaticQuery  } from "gatsby"
-import { StyledContainer, StyledProject, StyledImage, StyledContent, StyledLinkable } from './Project.styled'
+import { StyledContainer, StyledProject, StyledImage, StyledImageAlt, StyledContent, StyledContentAlt, StyledLinkable } from './Project.styled'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons"
 import Fade from 'react-reveal/Fade';
 
 const Projects = (props) => {
+    let counter = 0;
     const data = useStaticQuery(
         graphql`
             query {
@@ -28,31 +29,61 @@ const Projects = (props) => {
             }
         `
     )
+    
     return (
         <StyledContainer id="projects" className="container">
             <h1>#Lastest Work</h1>
             <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             {data.allContentfulProject.edges.map(({ node }) => {
-                return (
-                    <Fade left key={node.id}>
-                        <StyledProject className="row">
-                            <StyledImage background={`${node.logo.fluid.src}`} className="col-md-8" />
-                            <StyledContent className="col-md-4">
-                                <div className="information">
-                                    <h3>#{node.name}</h3>
-                                    <p>&mdash; {node.type}</p>
-                                </div>
+                counter++;
+                if(counter%2 === 0){
+                    return (
+                        <Fade left key={node.id}>
+                            <div className="project-holder">
+                            <StyledProject className="row">
+                                <StyledImage background={`${node.logo.fluid.src}`} className="col-md-8" />
+                                <StyledContent className="col-md-4">
+                                    <div className="information">
+                                        <h3>#{node.name}</h3>
+                                        <p>&mdash; {node.type}</p>
+                                    </div>
 
-                                <div className="linkable">
-                                    <StyledLinkable href={`${node.projectUrl}`}>
-                                        <span className="linkable__title">View Work</span>
-                                        <span><FontAwesomeIcon icon={faArrowAltCircleRight} color="#72D8FE" size="2x" /></span>
-                                    </StyledLinkable>
-                                </div>
-                            </StyledContent>
-                        </StyledProject>
-                    </Fade>
-                )
+                                    <div className="linkable">
+                                        <StyledLinkable href={`${node.projectUrl}`}>
+                                            <span className="linkable__title">View Work</span>
+                                            <span><FontAwesomeIcon icon={faArrowAltCircleRight} color="#72D8FE" size="2x" /></span>
+                                        </StyledLinkable>
+                                    </div>
+                                </StyledContent>
+                            </StyledProject>
+                            </div>
+                        </Fade>
+                    )
+                } else {
+                    return(
+                        <Fade right key={node.id}>
+                            <div className="project-holder">
+                            <StyledProject className="row">
+                                <StyledImageAlt background={`${node.logo.fluid.src}`} className="col-md-8" />
+                                <StyledContentAlt className="col-md-4">
+                                    <div className="information">
+                                        <h3>#{node.name}</h3>
+                                        <p>&mdash; {node.type}</p>
+                                    </div>
+
+                                    <div className="linkable">
+                                        <StyledLinkable href={`${node.projectUrl}`}>
+                                            <span className="linkable__title">View Work</span>
+                                            <span><FontAwesomeIcon icon={faArrowAltCircleRight} color="#72D8FE" size="2x" /></span>
+                                        </StyledLinkable>
+                                    </div>
+                                </StyledContentAlt>
+                                
+                            </StyledProject>
+                            </div>
+                        </Fade>
+                    )
+                }
             })}
         </StyledContainer>
     )
