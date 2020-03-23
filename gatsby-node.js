08a7e05cc3617af2461d4343bf822dfde4743f1e
Carlos <cfelixbeltran@dynamicagency.com>
2020-03-23 08:54:36 -0500

    REMOVED BlogPage creation due to SPA

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,39 +4,3 @@ const _ = require("lodash")
 // queries. We use the `graphql` function which returns a Promise
 // and ultimately resolve all of them using Promise.all(Promise[])
 
-exports.createPages = ({ actions, graphql }) => {
-	const { createPage } = actions;
-	const blogTemplate = path.resolve('src/templates/BlogPost/index.js');
-
-	// Individual blogs pages
-	const blogs = graphql(`
-		{
-            allContentfulBlogPost (limit:100) {
-                edges {
-                    node {
-                        id
-                        slug
-                    }
-                }
-            }
-		}
-	`).then(result => {
-		if (result.errors) {
-			Promise.reject(result.errors);
-		}
-
-		// Create blog pages
-		result.data.allContentfulBlogPost.edges.forEach((edge) => {
-            createPage ({
-                path: edge.node.slug,
-                component: blogTemplate,
-                context: {
-                slug: edge.node.slug
-                }
-            })
-        });
-	});
-
-	// Return a Promise which would wait for both the queries to resolve
-	return Promise.all([blogs]);
-};
