39f2a9bfb5b33d2dcdf1baf40c8a58d3bab055da
Carlos <cfelixbeltran@dynamicagency.com>
2020-03-29 13:52:47 -0500

    FIXED Container of Header Component

--- src/Components/Header/Header.styled.js
+++ src/Components/Header/Header.styled.js
@@ -2,16 +2,17 @@ import styled from 'styled-components';
 
 const StyledContainer = styled.div`
     display: flex;
-    margin-top: 3rem;
+    padding-top: 3rem;
     h1 {
         color: ${({ theme }) => theme.textActive};
     }
     h2, h3, h4, h5, h6{
         color: ${({ theme }) => theme.accentText};
     }
+    /*clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 100px));
+    background: ${({ theme }) => theme.angleBG};*/
 `
 const StyledImage = styled.img`
-
 `
 const StyledAuthorInfo = styled.div`
     h1{
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -30,17 +30,19 @@ const Header = (props) => {
     
     return (
         <Fragment>
-            <StyledContainer id="about" className="container">
-                <div className="row">
-                    <div className="col-lg-6 d-flex align-items-center">
-                        <StyledImage src={profile.fluid.src} />
+            <StyledContainer id="about">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 d-flex align-items-center">
+                            <StyledImage src={profile.fluid.src} />
+                        </div>
+                        
+                        <StyledAuthorInfo className="col-lg-6">
+                            <h1>HELLO</h1>
+                            <h2>I'm {name}</h2>
+                            <div dangerouslySetInnerHTML={{ __html: aboutMe.childMarkdownRemark.html }} />
+                        </StyledAuthorInfo>
                     </div>
-                    
-                    <StyledAuthorInfo className="col-lg-6">
-                        <h1>HELLO</h1>
-                        <h2>I'm {name}</h2>
-                        <div dangerouslySetInnerHTML={{ __html: aboutMe.childMarkdownRemark.html }} />
-                    </StyledAuthorInfo>
                 </div>
             </StyledContainer>
         </Fragment>
